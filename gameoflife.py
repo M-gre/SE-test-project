bd601d1394d2838e05f9895d5df69681e76fb4ae
M-gre <mathis.greve@gmail.com>
2020-11-18 17:45:10 +0100

    implemented init_gameboard, which initializes the array that represents the gameboard

--- gameoflife.py
+++ gameoflife.py
@@ -2,11 +2,14 @@ import numpy as np
 import pygame
 
 
-def init_surface(dim_x: int, dim_y: int, cellsize: int):
+def init_surface(dim_x: int, dim_y: int, cellsize: int) -> pygame.surface:
 	pygame.init()
 	pygame.display.set_caption("John Conway's Game of Life")
 	return pygame.display.set_mode((dim_x * cellsize, dim_y * cellsize))
 
+def init_gameboard(dim_x: int, dim_y: int) -> np.ndarray:
+	gameboard = np.zeros((dim_y, dim_x))
+	return gameboard
 
 if __name__ == '__main__':
 	pass
--- tests.py
+++ tests.py
@@ -5,12 +5,16 @@ import gameoflife as gol
 class GOLTestCase(unittest.TestCase):
 	def test_init_surface_size(self):
 		surface = gol.init_surface(100, 200, 3)
-		self.assertEqual(surface.get_width(), 100*3)
-		self.assertEqual(surface.get_height(), 200*3)
+		self.assertEqual(100 * 3, surface.get_width())
+		self.assertEqual(200 * 3, surface.get_height())
 
 	def test_init_surface_type(self):
 		with self.assertRaises(TypeError):
 			gol.init_surface("a", "b", None)
+
+	def test_init_gameboard_size(self):
+		gameboard = gol.init_gameboard(100, 200)
+		self.assertEqual((200, 100), gameboard.shape)
 
 
 if __name__ == '__main__':
