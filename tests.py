c2415d8c99e188e975c3231a00563000e4764139
M-gre <mathis.greve@gmail.com>
2020-11-18 17:20:09 +0100

    First Test for init_surface implemented init_surface, which opens up a game surface in appropriate size

--- tests.py
+++ tests.py
@@ -1,9 +1,16 @@
 import unittest
+import gameoflife as gol
 
 
-class MyTestCase(unittest.TestCase):
-	def test_something(self):
-		self.assertEqual(True, True)
+class GOLTestCase(unittest.TestCase):
+	def test_init_surface_size(self):
+		surface = gol.init_surface(100, 200, 3)
+		self.assertEqual(surface.get_width(), 100*3)
+		self.assertEqual(surface.get_height(), 200*3)
+
+	def test_init_surface_type(self):
+		with self.assertRaises(TypeError):
+			gol.init_surface("a", "b", None)
 
 
 if __name__ == '__main__':
