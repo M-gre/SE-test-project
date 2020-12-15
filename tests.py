9f5a7e1dd960d26f99b5622698d35e92a771da0f
M-gre <mathis.greve@gmail.com>
2020-12-15 23:57:05 +0100

    demo commit for presentation, tests fail

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import gameoflife as gol
 
 class GOLTestCase(unittest.TestCase):
 	def test_init_surface_size(self):
-		try:  # travis fails because the docker container has no video device to display the surface
+		try:  # travis fails because the build worker has no video device to display the surface
 			surface = gol.init_surface(100, 200, 3)
 			self.assertEqual(100 * 3, surface.get_width())
 			self.assertEqual(200 * 3, surface.get_height())
@@ -12,7 +12,7 @@ class GOLTestCase(unittest.TestCase):
 			pass
 
 	def test_init_surface_type(self):
-		try:  # travis fails because the docker container has no video device to display the surface
+		try:  # travis fails because the build worker has no video device to display the surface
 			with self.assertRaises(TypeError):
 				gol.init_surface("a", "b", None)
 		except gol.pygame.error:
@@ -44,12 +44,19 @@ class GOLTestCase(unittest.TestCase):
 								[0, 1, 0]
 								])
 		next_pattern = gol.np.array([[0, 0, 0],
-									[0, 0, 1],
-									[0, 1, 0]
-									])
+									 [0, 0, 1],
+									 [0, 1, 0]
+									 ])
 		gameboard = gol.init_gameboard(3, 3, pattern)
 		updated_gameboard = gol.update(gameboard)
 		self.assertTrue((next_pattern == updated_gameboard).all())
+
+	def test_set_random_pattern(self):
+		col = 5
+		row = 10
+		result = gol.set_random_pattern(col, row)
+		self.assertEqual(col, result.shape[0])
+		self.assertEqual(row, result.shape[1])
 
 
 if __name__ == '__main__':
